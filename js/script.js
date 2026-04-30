404075a87c9affeececbc65053818b66c6d93472
allex1930 <allexoliveiraa756@gmail.com>
2026-04-30 20:37:06 -0300

    exercicio01

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let nome = "Allex";
+let idade = 21;
+const cidade = "São Bernardo do Campo";
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`);
+
+console.log(typeof nome);      
+console.log(typeof idade);     
+console.log(typeof true);      
+
+
+const base = 8;
+const altura = 4;
+const area = base * altura;
+
+console.log(`area: ${area}.`);
+
+
+console.log(0 == "0");   
+console.log(0 === "0");  
