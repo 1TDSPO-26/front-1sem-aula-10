69e01956b4e0f6141ee0fdd6ca989324ccf8ef61
caiofeixas <caiofeixas5@gmail.com>
2026-04-30 20:36:29 -0300

    exercicio 1 da aula de js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let nome = "Caio"
+let idade = 22
+const cidade = "São Paulo"
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`)
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof true)
+console.log(typeof "JS")
+const base = 10
+const altura = 5
+let area = base * altura
+console.log("Área do retângulo:", area)
+console.log(0 == "0")
+console.log(0 === "0")
