0806ce99be89658863a66f051b6fb17e5a672ac7
Enzo-N-Freitas <enzo_negrao_freitas@outlook.com>
2026-04-30 20:47:33 -0300

    testando javascript

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const nome = "Enzo";
+const idade = 19; 
+const cidade = "São Paulo";
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e moro em ${cidade}.`);
+
+console.log(typeof "string");
+console.log(typeof 19);
+console.log(typeof null);
+console.log(typeof true);
+
+const base = 10;
+const altura = 20;
+
+console.log(`Área do triângulo: ${base * altura}`);
+
+if (0 == "0") console.log("Diferente");
+if (0 === "0") console.log("Igual")
+
