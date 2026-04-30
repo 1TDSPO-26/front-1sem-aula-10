f46ab474fe8cf73f703a3e45eaaa3c1af686c0d8
MuriloSCruzz <cruzmurilo23@gmail.com>
2026-04-30 20:40:40 -0300

    feat: Fazendo calculos, atribuindo valores e utilizando funções em JavaScript

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const nome = "Murilo";
+const idade = 18;
+let cidade = "São Paulo";
+
+console.log(`Olá ${nome}! Você tem ${idade} anos, o nome da sua cidade é ${cidade}`);
+
+typeof(nome);
+typeof(idade);
+typeof(true);
+typeof(cidade);
+
+const base_retangulo = 5
+const altura_retangulo = 10
+
+console.log(`A base do retângulo é ${base_retangulo}, a altura é ${altura_retangulo}, sendo assim, sua área é ${base_retangulo * altura_retangulo}`);
+
+console.log(0 == "0");
+console.log(0 === "0");
+
+
+
