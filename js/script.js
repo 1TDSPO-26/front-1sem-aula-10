56ba08854bea9ba858776f5bd8ce9a58399310e6
ropark-tech <ropark.tech@proton.me>
2026-05-06 23:02:10 -0300

    Feat: Criação e exemplos de form, semantica e tratamento de css e js. criação da pagina login.

--- js/script.js
+++ js/script.js
@@ -106,52 +106,109 @@
 // console.log(0 == "0")
 // console.log(0 === "0")
 
-const nota = 72;
+// const nota = 72;
  
-if (nota >= 90) {
-    console.log("Aprovado com distinção");
-} else if (nota >= 60) {
-    console.log("Aprovado");
-} else {
-    console.log("Reprovado");
-}
+// if (nota >= 90) {
+//    console.log("Aprovado com distinção");
+// } else if (nota >= 60) {
+//    console.log("Aprovado");
+// } else {
+//    console.log("Reprovado");
+// }
  
 // Operador ternário (if/else em uma linha):
-const resultado = nota >= 60 ? "Aprovado" : "Reprovado";
+// const resultado = nota >= 60 ? "Aprovado" : "Reprovado";
 
 // switch
 // Use switch quando comparar uma variável contra múltiplos valores fixos. Mais legível que muitos else if encadeados.
-const diaSemana = "segunda";
+// const diaSemana = "segunda";
 
-switch (diaSemana) {
-  case "segunda":
-    console.log("Dia útil e trabalho");
-    break;
-  case "terça":
-    console.log("Dia útil e dia de aula");
-    break;
-  case "quarta":
-    console.log("Dia útil e dia de feira");
-    break;
-  case "quinta":
-    console.log("Dia útil e dia de festa");
-    break;
-  case "sexta":
-    console.log("Feriado!!!");
-    break;
-  case "sábado":
-    console.log("Fim de semana!");
-    break;
-  case "domingo":
-    console.log("Fim de semana!");
-    break;
-  default:
-    console.log("Dia inválido");
-}
+// switch (diaSemana) {
+//   case "segunda":
+//     console.log("Dia útil e trabalho");
+//     break;
+//   case "terça":
+//     console.log("Dia útil e dia de aula");
+//     break;
+//   case "quarta":
+//    console.log("Dia útil e dia de feira");
+//     break;
+//   case "quinta":
+//     console.log("Dia útil e dia de festa");
+//     break;
+//   case "sexta":
+//     console.log("Feriado!!!");
+//     break;
+//   case "sábado":
+//     console.log("Fim de semana!");
+//     break;
+//   case "domingo":
+//     console.log("Fim de semana!");
+//    break;
+//   default:
+//     console.log("Dia inválido");
+// }
 
 // Nunca esqueça o break! Sem ele o JS continua executando os cases seguintes (fall-through).
 // EXERCÍCIO — Módulo 2
 // •	Crie um verificador de faixa etária: "criança" (< 12), "adolescente" (12–17), "adulto" (18–59), "idoso" (60+).
 // •	Simule um semáforo com switch: verde, amarelo, vermelho → imprima a instrução correspondente.
 // •	Crie um mini-calculadora: peça dois números e uma operação (+, -, *, /) e exiba o resultado.
+
+
+// Crie um verificador de faixa etária: "criança" (< 12), "adolescente" (12–17), "adulto" (18–59), "idoso" (60+).
+// let idade = 35
+
+// if(idade < 12){
+//   console.log("Criança")
+// }else if(idade >= 12 && idade <= 17){
+//   console.log("Adolecente");
+// }else if(idade >= 18 && idade <= 59){
+//   console.log("Adulto");
+// }else{
+//   console.log("Idoso");
+// }
+
+// Simule um semáforo com switch: verde, amarelo, vermelho → imprima a instrução correspondente.
+// let semaforo = "vermelho";
+
+// switch (semaforo) {
+//   case "verde":
+//     console.log("Prossiga");
+//     break;
+//  case "amarelo":
+//     console.log("Reduza");
+//     break;
+//   case "vermelho":
+//     console.log("Pare!");
+//     break;
+//   default:
+//     console.log("Semáforo com defeito!");
+// }
+
+// Crie um mini-calculadora: peça dois números e uma operação (+, -, *, /) e exiba o resultado.
+// let num_1 = 12;
+// let num_2 = 24;
+// let operacao = "soma";
+
+// switch (operacao) {
+//   case "+":
+//     const soma = num_1 + num_2;
+//     console.log(soma);
+//     break;
+//   case "-":
+//     const subtracao = num_1 - num_2;
+//     console.log(subtracao);
+//     break;
+//   case "*":
+//     const multiplicacao = num_1 * num_2;
+//     console.log(multiplicacao);
+//     break;
+//   case "/":
+//     const resultado = nr2 !== 0 ? nr1 / nr2 : "Erro divisão por zero.";
+//     console.log(resultado);
+//      break;
+//   default:
+//    console.log("Operação Invalida")
+// }
 
