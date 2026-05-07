21376c1cbd79fca433b04a5369d2c496163100ce
ropark-tech <ropark.tech@proton.me>
2026-05-07 20:41:06 -0300

    Feat: Montando recuperador de elementos e Sistema de login com recuperacao de dados no evento click e tratamento de erros em try catch

--- js/script.js
+++ js/script.js
@@ -212,3 +212,62 @@
 //    console.log("Operação Invalida")
 // }
 
+// Recuperando o botão entrar
+// const botaoEntrar = document.getElementById("btnEntrar");
+
+// // Atrelar ao botão um evento
+// // Vamos utilizar uma função que escuta eventos.
+// // Essa função se chama addEventListener(param1, param2) e recebe dois parâmetros:
+// // O primeiro é o evento em si (click ou outro qualquer).
+// // O segundo é a função, que pode ser uma função anônima ou uma função declarada.
+// // Que será executada quando esse evento acontecer.
+
+// // Ex: botaoEntrar.addEventListener("click", function(){ Coisas aqui dentro da função para serem executadas quando o botão for clicado. });
+
+// botaoEntrar.addEventListener("click",function(){
+//     console.log("O botão foi clicado!");
+// });
+
+//Exemplo de função
+
+// function nomeDaFuncao(){
+//     console.log("Olla");
+// }
+
+
+// Montando um objeto para comparar os dados do formulario
+const usuario = {
+    nome : "Zezinho",
+    email : "zezinho@gmail.com",
+    senha : "123456"
+};
+
+
+
+const botaoEntrar = document.getElementById("btnEntrar");
+
+
+botaoEntrar.addEventListener("click", function(evento){
+    evento.preventDefault();
+
+    try{
+        // Recuperando os 2 campos do formulário de login e imprimir no console:
+        const email = document.getElementById("idEmail");
+        const senha = document.getElementById("idSenha");
+        // Imprimindo o valor dos campos em tela.
+        console.log(email.value);
+        console.log(senha.value);
+        //Vamos comparar os dados digitados no formulário com os dados do objeto "usuario" e caso seja valido, redirecionar para a página index.html.
+        if(!(email === null && senha === null)){
+            if(email.value === usuario.email && senha.value ===usuario.senha){
+                window.location.href = "./index.html";
+            }else{
+                throw new Error("Preencha corretamente!");
+            }
+        }
+    }catch(error){
+        alert(error.message);
+    }
+
+}); 
+
