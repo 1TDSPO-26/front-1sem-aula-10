9e5806ca8408923124bd0b4a61b3b9686da6c99d
thalesmj <finethales@gmail.com>
2026-05-06 21:58:16 -0300

    feat: Resolução de exercícios

--- js/script.js
+++ js/script.js
@@ -34,3 +34,55 @@ switch (diaSemana) {
 // •	Simule um semáforo com switch: verde, amarelo, vermelho → imprima a instrução correspondente.
 // •	Crie um mini-calculadora: peça dois números e uma operação (+, -, *, /) e exiba o resultado.
 
+let idade = 25;
+
+if(idade<12){
+  console.log("Criança");
+}
+else if(idade>=12 && idade<=17){
+  console.log("Adolecente")
+}
+else if (idade>18 && idade<=59){
+  console.log("Adulto")
+}
+else{
+  console.log("Idoso")
+}
+
+let semaforo = "roxo";
+
+  switch(semaforo){
+    case "verde":
+      console.log("Siga");
+      break;
+    case "amarelo":
+      console.log("Atenção");
+      break;
+    case "vermelho":
+      console.log("Pare");
+      break;
+    default:
+      console.log("Semáforo com defeito!");
+  }
+
+  let nr1 = 10;
+  let nr2 = 20;
+  let operacao = "+";
+
+  switch(operacao){
+    case "+":
+      console.log(nr1+nr2);
+      break
+    case "-":
+      console.log(nr1-nr2);
+      break
+    case "*":
+      console.log(nr1*nr2);
+      break
+    case "/":
+      const resultado = nr2!==0 ? nr1/nr2: "Não é possível dividir por zero";
+      console.log(resultado);
+      break
+    default:
+      console.log("Operação invalida!");
+  }
