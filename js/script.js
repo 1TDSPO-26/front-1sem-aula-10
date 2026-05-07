c30d8b5a53ea1bf743ea387f1ba4bbe042171071
allex1930 <allexoliveiraa756@gmail.com>
2026-05-07 20:07:28 -0300

    feat : primeira manipulação do DOM, atraves do elemento buttom e evento click

--- js/script.js
+++ js/script.js
@@ -1,51 +1,21 @@
-let idade = 17
-
-if(idade < 12){
-    console.log("Crianca")
-}else if(idade >= 12 && idade <= 17){
-    console.log("Adolescente")
-}else if(idade >= 18 && idade <= 59){
-    console.log("Adulto")
-}else{
-    console.log("Idoso")
-}
 
 
-let semaforo = "vermelho";
+//Recuperando o botão entrar
+const botaoEntrar = document.getElementById("btnEntrar");
 
-switch(semaforo){
-  case "verde":
-    console.log("Siga");
-    break;
-  case "amarelo":
-    console.log("Atenção");
-    break;
-  case "vermelho":
-    console.log("Pare");
-    break;
-  default:
-    console.log("Semáforo inválido");
-}
+//Atrelar ao botão um evento
+//Vamos utilizar uma função que escuta eventos.
+//Essa função se chama addEventListener(param1, param2) e recebe dois parâmetros:
+//O primeiro é o evento em si (click ou outro qualquer).
+//O segundo é a função, que pode ser uma função anônima ou uma função declarada.
+//Que será executada quando esse evento acontecer.
+
+//Ex: botaoEntrar.addEventListener("click", function(){ Coisas aqui dentro da função para serem executadas quando o botão for clicado. });
+
+botaoEntrar.addEventListener("click", function(){
+    console.log("O botão foi clicado!");
+});
 
 
-// let nr1 = 10;
-// let nr2 = 20;
-// let operacao = "x";
 
-// switch(operacao){
-//   case "+":
-//     console.log(nr1 + nr2);
-//     break;
-//   case "-":
-//     console.log(nr1 - nr2);
-//     break;  
-//   case "*":
-//     console.log(nr1 * nr2);
-//     break;
-//   case "/":
-//     const resultado = nr2 !== 0 ? nr1 / nr2 : "Erro divisão por zero.";
-//     console.log(resultado);
-//     break;
-//   default:
-//     console.log("Operação inválida!");
-// }
+
