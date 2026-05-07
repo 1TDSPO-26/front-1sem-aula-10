f1f976be16e99461972b7daba5d44d47bfcd82ab
MuriloSCruzz <cruzmurilo23@gmail.com>
2026-05-07 20:41:11 -0300

    feat: aplicando eventos em sistema de login

--- js/script.js
+++ js/script.js
@@ -1,21 +1,61 @@
-const nome = "Murilo";
-const idade = 18;
-let cidade = "São Paulo";
-
-console.log(`Olá ${nome}! Você tem ${idade} anos, o nome da sua cidade é ${cidade}`);
-
-typeof(nome);
-typeof(idade);
-typeof(true);
-typeof(cidade);
-
-const base_retangulo = 5
-const altura_retangulo = 10
-
-console.log(`A base do retângulo é ${base_retangulo}, a altura é ${altura_retangulo}, sendo assim, sua área é ${base_retangulo * altura_retangulo}`);
-
-console.log(0 == "0");
-console.log(0 === "0");
+//Montando um objeto para comparar com os dados do formulário
+const usuario = {
+    nome : "Zezinho", 
+    email : "zezinho@gmail.com.br",
+    senha : "zezinho123"
+}
 
 
 
+//Recuperando o botão entrar
+const botaoEntrar = document.getElementById("btnEntrar");
+
+
+//Atrelar ao botão um evento
+//Vamos utilizar uma função que escuta eventos.
+//Essa função se chama addEventListeener(param1, param2) e recebe dois parâmetros:
+//O primeiro é o evento em si (click ou outro qualquer).
+//O segundo é a função, que pode ser uma função anônima ou uma função declarada.
+//Que será executada quando esse evento acontecer.
+
+//Ex: botaoEntrar.addEventListener("click", function(){ Coisas aqui dentro da função para serem executadas quando o botão for clicado. });
+
+botaoEntrar.addEventListener("click", function(evento){
+    evento.preventDefault();
+
+
+
+try {
+
+  //Recuperando os 2 campos do formulário de login e impirmir no console:    
+const campoEmail = document.getElementById("idEmail");
+const campoSenha = document.getElementById("idSenha");
+    //Imprimindo o valor dos campos em tela
+console.log(campoEmail.value);
+console.log(campoSenha.value);
+
+//Vamos comparar os dados digitados no formulário com os dados do objeto "usuario" e caso seja valido, redirecionar para a página index.html.
+    
+if(!(campoEmail === null && campoSenha === null )){
+    if(campoEmail.valeu === usuario.campoEmail && campoSenha.valeu === usuario.campoSenha){
+        window.location.href = "./index.html";
+    }else{
+        throw new Error("Preencha os campos corretamente!");
+    }
+}
+
+
+} catch (error){
+
+
+}
+});
+
+
+//Exemplo de função
+//function nomeDaFuncao() {
+  //  console.log("Olla");
+    //return "Olá";
+//}
+
+//nomeDaFuncao();
