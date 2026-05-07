f9f95f8e2608adf02751df4795d78f4580edab6a
alecarlosjesus <profalexandre.jesus@email.com.br>
2026-05-07 20:40:34 -0300

    feat : Sistema de login com recuperação de dados no evento click e tratamento de erros em try catch.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@email.com.br>

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-
+//Montando um objeto para comparar com os dados do formulário
+const usuario = {
+  nome: "Zezinho",
+  email: "zezinho@gmail.com",
+  senha: "123456",
+};
 
 //Recuperando o botão entrar
 const botaoEntrar = document.getElementById("btnEntrar");
@@ -12,10 +17,31 @@ const botaoEntrar = document.getElementById("btnEntrar");
 
 //Ex: botaoEntrar.addEventListener("click", function(){ Coisas aqui dentro da função para serem executadas quando o botão for clicado. });
 
-botaoEntrar.addEventListener("click", function(){
-    console.log("O botão foi clicado!");
+botaoEntrar.addEventListener("click", function (evento) {
+  evento.preventDefault();
+
+  try {
+    //Recuperando os 2 campos do formulário de login e imprimir no console:
+    const email = document.getElementById("idEmail");
+    const senha = document.getElementById("idSenha");
+
+    //Imprimindo o valor dos campos em tela.
+    console.log(email.value);
+    console.log(senha.value);
+
+    //Vamos comparar os dados digitados no formulário com os dados do objeto "usuario" e caso seja valido, redirecionar para a página index.html.
+
+    if (!(email === null && senha === null)) {
+      if (email.value === usuario.email && senha.value === usuario.senha) {
+        window.location.href = "./index.html";
+      } else {
+        throw new Error("Nome de usuário ou senha incorretos!");
+      }
+    } else {
+      throw new Error("Preencha os campos corretamente!");
+    }
+    
+  } catch (error) {
+    alert(error.message);
+  }
 });
-
-
-
-
